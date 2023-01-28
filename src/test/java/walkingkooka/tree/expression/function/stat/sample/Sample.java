53df10249c80b027a93143dc97bad55c215be558
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-01-28 14:44:18 +1100

    Stat.gwt.xml

--- src/test/java/walkingkooka/tree/expression/function/stat/sample/Sample.java
+++ src/test/java/walkingkooka/tree/expression/function/stat/sample/Sample.java
@@ -17,7 +17,53 @@
 
 package walkingkooka.tree.expression.function.stat.sample;
 
+import org.junit.jupiter.api.Assertions;
+import walkingkooka.Either;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.tree.expression.ExpressionNumberKind;
+import walkingkooka.tree.expression.FakeExpressionEvaluationContext;
+import walkingkooka.tree.expression.function.stat.StatExpressionFunctions;
+
+import java.math.MathContext;
+
 public final class Sample {
     public static void main(final String[] args) {
+        final ExpressionNumberKind kind = ExpressionNumberKind.BIG_DECIMAL;
+
+        Assertions.assertEquals(
+                kind.create(3),
+                StatExpressionFunctions.count()
+                        .apply(
+                                Lists.of(
+                                        kind.create(123),
+                                        kind.create(456),
+                                        kind.create(789)
+                                ),
+                                new FakeExpressionEvaluationContext() {
+                                    @Override
+                                    public ExpressionNumberKind expressionNumberKind() {
+                                        return kind;
+                                    }
+
+                                    @Override
+                                    public MathContext mathContext() {
+                                        return MathContext.DECIMAL128;
+                                    }
+
+                                    @Override
+                                    public <TT> Either<TT, String> convert(final Object value,
+                                                                           final Class<TT> target) {
+                                        try {
+                                            return this.successfulConversion(
+                                                    (TT) kind.create((Number) value),
+                                                    target
+                                            );
+                                        } catch (final Exception fail) {
+                                            return this.failConversion(value, target);
+                                        }
+                                    }
+                                }
+                        )
+        );
     }
 }
